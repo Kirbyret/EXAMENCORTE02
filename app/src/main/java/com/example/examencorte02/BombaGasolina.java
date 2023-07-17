1420bede0b330110fa04f9b99563cb389d777cda
Kirbyret <2020030313@upsin.edu.mx>
2023-07-17 12:39:15 -0700

    Bombagasolina

---
+++ app/src/main/java/com/example/examencorte02/BombaGasolina.java
@@ -0,0 +1,11 @@
+package com.example.examencorte02;
+
+public class BombaGasolina {
+
+    private int id;
+    private int numBomba;
+    private int capacidadBomba;
+    private int tipoGasolina;
+    private double precio;
+    private int acumuladoLitrosBomba;
+}
